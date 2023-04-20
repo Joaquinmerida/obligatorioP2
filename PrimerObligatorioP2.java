8ad84479034144805a3016e2a9d8c202a352bf57
Joaquin Merida <93019476+Joaquinmerida@users.noreply.github.com>
2023-04-20 18:26:05 -0300

    Primer commit

---
+++ PrimerObligatorioP2.java
@@ -0,0 +1,15 @@
+
+package primerobligatoriop2;
+
+/**
+ *
+ * 
+ */
+public class PrimerObligatorioP2 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
